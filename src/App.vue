fa10a3f7acb8fe8a425d151f8c4518341f4481c4
Samet Çetin <samet.cetin@n2mobil.com.tr>
2021-06-22 17:32:18 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <app-memory>
+          <h1 slot="heading" class="text-center">It was great day</h1>
+          <p slot="content" class="text-center">This was a goog memory....</p>
+        </app-memory>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Memory from './Memory';
+  export default {
+    components: {
+      appMemory: Memory
+    }
+  }
+</script>
+
